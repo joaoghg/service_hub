482e0c25829212f87531e07e531c9540f21f9506
joaoghg <jherreiragarnica@gmail.com>
2024-07-10 21:27:36 -0300

    assets

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const LoginScreen = () => {
+  const insets = useSafeAreaInsets()
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View
+      style={{
+        paddingTop: insets.top
+      }}
+    >
+      <Image source={require('../../assets/images/login.png')} />
     </View>
   )
 }
