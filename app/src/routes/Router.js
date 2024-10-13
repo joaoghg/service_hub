3e9a06109d75b1b4fe8f5b8d580f17f3e2168844
joaoghg <jherreiragarnica@gmail.com>
2024-10-13 20:44:51 -0300

    Direcionando stacks

--- app/src/routes/Router.js
+++ app/src/routes/Router.js
@@ -4,22 +4,27 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Loading from '../screens/Loading'
 import { NavigationContainer } from '@react-navigation/native'
+import AuthStack from './AuthStack'
+import AppStack from './AppStack'
 
 export default function Router(){
 
     const insets = useSafeAreaInsets()
-
-    let screen = <Loading />
+    const [screen, setScreen] = useState(<Loading />)
     
     const verifyToken = async () => {
         const token =  await AsyncStorage.getItem('authToken')
 
-        // if(token === null){
-        //     screen = 'AuthStack'
-        // }
-        // else{
-        //     screen = 'AppStack'
-        // }
+        if(token === null){
+            setTimeout(() => {
+                setScreen(<AuthStack />)
+            }, 2000)
+        }
+        else{
+            setTimeout(() => {
+                setScreen(<AppStack />)
+            }, 2000)
+        }
     }
 
     useEffect(() => {
