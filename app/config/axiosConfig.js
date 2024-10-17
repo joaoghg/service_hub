58d10c411509440ae2d6d7d41f6c302be467142d
joaoghg <jherreiragarnica@gmail.com>
2024-10-16 23:31:33 -0300

    config

--- app/config/axiosConfig.js
+++ app/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `http://172.29.11.88:4000/api`,
+    baseURL: `http://192.168.0.118:4000/api`,
     timeout: 20000,
     headers: { 
         'Accept': 'application/json' 
